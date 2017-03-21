9e814f840289543123fdf97b787af94da02aa58d
John Doe <webdevnorth@gmail.com>
2017-03-21 00:10:02 +0000

    add tests, edge-case handling to linkedListStack

--- princeton_1/stacks_&_queues/linkedListStack.js
+++ princeton_1/stacks_&_queues/linkedListStack.js
@@ -11,10 +11,12 @@ var protoStack = {
 		this._length++;
 	},
 	pop: function() {
-		var oldHead = this.head;
-		this.head = oldHead.next;
-		this._length--;
-		return oldHead;
+		if (this._length > 0) {
+			var oldHead = this.head;
+			this.head = oldHead.next;
+			this._length--;
+			return oldHead.data;
+		}
 	},
 	isEmpty: function() {
 		return this._length === 0;
@@ -36,3 +38,21 @@ function factoryStack() {
 	stack.head = null;
 	return stack;
 }
+
+var test = factoryStack();
+console.log(test);
+test.push(1);
+test.push(2);
+test.push(3);
+test.push(4);
+test.push(5);
+console.log(JSON.stringify(test, null, 2));
+console.log(test.pop());
+console.log(test.isEmpty());
+test.pop();
+test.pop();
+test.pop();
+test.pop();
+console.log(test.isEmpty());
+console.log(test.pop());
+console.log(test);
