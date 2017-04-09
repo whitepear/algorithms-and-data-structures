c374a8123f4d38d0520a2f08fd8659702df34a8a
John Doe <webdevnorth@gmail.com>
2017-04-10 00:12:57 +0100

    create hashing function

---
+++ algorithms/hashingFunction.js
@@ -0,0 +1,24 @@
+// hashing function that produces integers based on input.
+// hashing process differs based on input type.
+// strings, numbers and booleans are supported.
+
+module.exports = function(val) {
+	// determine type of value passed
+	if (typeof val === "number") {
+		// return 32-bit integer
+		return val | 0;	
+
+	} else if (typeof val === "boolean") {
+		if (val) return 1231;
+		else 		 return 1237;
+
+	} else if (typeof val === "string") {
+		// compute hash using Horner's method
+		var hash = 0;
+		for (var i = 0; i < val.length; i++) {
+			hash = val.charCodeAt(i) + (31 * hash);
+		}
+		return hash | 0;
+
+	}
+}
