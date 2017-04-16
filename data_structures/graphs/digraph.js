1e66613e7bb8dd7cf10df9f519c057a6dfa3eee1
John Doe <webdevnorth@gmail.com>
2017-04-16 19:54:28 +0100

    add reverse method to digraph

--- data_structures/graphs/digraph.js
+++ data_structures/graphs/digraph.js
@@ -6,6 +6,18 @@ var protoDigraph = {
 	addEdge: function(v, w) {
 		// add edge for vertex v to w
 		this.adj[v].add(w);
+	},
+	reverse: function() {
+		// return reverse of digraph
+		var reverse = factoryDigraph(this.V);
+		for (var v = 0; v < this.V; v++) {
+			var currentVertex = this.adj[v].head;
+			while (currentVertex !== null) {
+				reverse.addEdge(currentVertex.data, v);
+				currentVertex = currentVertex.next;
+			}
+		}
+		return reverse;
 	}
 };
 
@@ -20,20 +32,50 @@ function factoryDigraph(V) {
 	return digraph;
 }
 
-// Exported adjacency list
-var foo = factoryDigraph(7);
-foo.addEdge(0,5);
-foo.addEdge(0,2);
-foo.addEdge(0,1);
-foo.addEdge(3,6);
-foo.addEdge(3,5);
-foo.addEdge(3,4);
-foo.addEdge(5,2);
-foo.addEdge(6,4);
-foo.addEdge(6,0);
-foo.addEdge(3,2);
-foo.addEdge(1,4);
-module.exports = foo;
+// Exported adjacency lists
+// dag
+var dag = factoryDigraph(7);
+dag.addEdge(0,5);
+dag.addEdge(0,2);
+dag.addEdge(0,1);
+dag.addEdge(3,6);
+dag.addEdge(3,5);
+dag.addEdge(3,4);
+dag.addEdge(5,2);
+dag.addEdge(6,4);
+dag.addEdge(6,0);
+dag.addEdge(3,2);
+dag.addEdge(1,4);
+
+// strongly-connected component-containing graph
+var stronglyConnected = factoryDigraph(13);
+stronglyConnected.addEdge(0,1);
+stronglyConnected.addEdge(0,5);
+stronglyConnected.addEdge(2,0);
+stronglyConnected.addEdge(2,3);
+stronglyConnected.addEdge(3,2);
+stronglyConnected.addEdge(3,5);
+stronglyConnected.addEdge(4,2);
+stronglyConnected.addEdge(4,3);
+stronglyConnected.addEdge(5,4);
+stronglyConnected.addEdge(6,0);
+stronglyConnected.addEdge(6,4);
+stronglyConnected.addEdge(6,8);
+stronglyConnected.addEdge(6,9);
+stronglyConnected.addEdge(7,6);
+stronglyConnected.addEdge(7,9);
+stronglyConnected.addEdge(8,6);
+stronglyConnected.addEdge(9,10);
+stronglyConnected.addEdge(9,11);
+stronglyConnected.addEdge(10,12);
+stronglyConnected.addEdge(11,4);
+stronglyConnected.addEdge(11,12);
+stronglyConnected.addEdge(12,9);
+
+module.exports = {
+	dag: dag,
+	stronglyConnected: stronglyConnected
+};
 
 // Tests
 // var test = factoryDigraph(50);
