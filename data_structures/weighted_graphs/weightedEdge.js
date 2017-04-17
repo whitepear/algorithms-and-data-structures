3438ea9428bea74c5c24b8f27a97c0a6af7aa0be
John Doe <webdevnorth@gmail.com>
2017-04-17 10:43:33 +0100

    create weighted-edge factory

---
+++ data_structures/weighted_graphs/weightedEdge.js
@@ -0,0 +1,27 @@
+// API for creating weighted edges in a graph.
+
+var protoEdge = {
+	either: function() {
+		return this.v;
+	},
+	other: function(vertex) {
+		if (vertex === this.v) return w;
+		else return v;
+	},
+	compareTo: function(otherEdge) {
+		if 			(this.weight < otherEdge.weight) return -1;
+		else if (this.weight > otherEdge.weight) return 1;
+		else 																		 return 0;
+	}
+};
+
+function factoryEdge(v, w, weight) {
+	var edgeObj = Object.create(protoEdge);
+	edgeObj.v = v; // vertex
+	edgeObj.w = w; // other vertex
+	edgeObj.weight = weight; // edge weight
+
+	return edgeObj;
+}
+
+module.exports = factoryEdge;
