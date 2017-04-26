54955bb6393d43e86c8e79fd0b0bfb285dfd0219
John Doe <webdevnorth@gmail.com>
2017-04-26 13:43:37 +0100

    add additional operations to ternary tree

--- data_structures/trees/ternarySearchTrie.js
+++ data_structures/trees/ternarySearchTrie.js
@@ -1,3 +1,5 @@
+var factoryQueue = require('../stacks_queues/arrayQueue.js');
+
 var protoTrie = {
 	factoryNode: function() {
 		return {
@@ -55,6 +57,91 @@ var protoTrie = {
 				return currentNode;
 			}
 		}
+	},
+	keys: function() {
+		// returns all keys in symbol table
+		if (this.root) {
+			var queue = factoryQueue();
+			collect(this.root, '', queue);
+			return queue;
+		}
+
+		// all keys in subtrie rooted at x with given prefix
+		function collect(currentNode, prefix, queue) {
+			if (currentNode === null) return;
+
+			collect(currentNode.left, prefix, queue);
+			if (currentNode.val !== undefined) {
+				queue.enqueue(prefix + currentNode.char);
+			}
+			prefix = prefix + currentNode.char;
+			collect(currentNode.mid, prefix, queue);
+			prefix = prefix.slice(0, -1);
+			collect(currentNode.right, prefix, queue);
+		}
+	},
+	keysWithPrefix: function(prefix) {
+		// collect keys in subtrie rooted at prefix
+		var queue = factoryQueue();
+		var rootNode = get(this.root, prefix, 0);
+		if (rootNode === null) return queue;
+		if (rootNode.val !== undefined) queue.enqueue(prefix);
+
+		collect(rootNode.mid, prefix, queue);
+		return queue;
+
+
+		function get(currentNode, key, d) {
+			if (currentNode === null || currentNode === undefined) return null;
+
+			var char = key[d];
+			if (char < currentNode.char) {
+				return get(currentNode.left, key, d);
+			} else if (char > currentNode.char) {
+				return get(currentNode.right, key, d);
+			} else if (d < key.length-1) {
+				// if equal and not at key's end, go mid
+				return get(currentNode.mid, key, d+1);
+			} else {
+				// if equal and at key's end, return node
+				return currentNode;
+			}
+		}
+
+		// all keys in subtrie rooted at x with given prefix
+		function collect(currentNode, prefix, queue) {
+			if (currentNode === null) return;
+
+			collect(currentNode.left, prefix, queue);
+			if (currentNode.val !== undefined) {
+				queue.enqueue(prefix + currentNode.char);
+			}
+			prefix = prefix + currentNode.char;
+			collect(currentNode.mid, prefix, queue);
+			prefix = prefix.slice(0, -1);
+			collect(currentNode.right, prefix, queue);
+		}		
+	},
+	longestPrefixOf: function(query) {
+		// search for query string, keeping track of longest
+		// key encountered
+		var length = 0;
+		var currentNode = this.root;
+		var i = 0;
+		while (currentNode !== null && i < query.length) {
+			var char = query[i];
+			if (char < currentNode.char) {
+				currentNode = currentNode.left;
+			} else if (char > currentNode.char) {
+				currentNode = currentNode.right;
+			} else {
+				i++;
+				if (currentNode.val !== undefined) length = i;
+				currentNode = currentNode.mid;
+			}
+		}
+
+		return query.slice(0, length);
 	}
 }
 
@@ -68,6 +155,7 @@ var test = factoryTrie();
 console.log('Test on initialization: ', test);
 console.log('Contains miss: ', test.contains('foo'));
 console.log('Get miss: ', test.get('foo'));
+console.log('Empty .keys() check: ', test.keys());
 test.put('foo', 1);
 console.log('Test post-insert: ', JSON.stringify(test, null, 2));
 console.log('Contains hit: ', test.contains('foo'));
@@ -76,3 +164,16 @@ test.put('far', 2);
 console.log('Test post-insert: ', JSON.stringify(test, null, 2));
 console.log('Contains hit: ', test.contains('far'));
 console.log('Get hit: ', test.get('far'));
+test.put('zebra', 3);
+test.put('blood', 4);
+test.put('satellite', 5);
+test.put('brooklyn', 6);
+console.log('.keys() check: ', JSON.stringify(test.keys(), null, 2));
+test.put('open', 7);
+test.put('opal', 8);
+test.put('opus', 9);
+test.put('oprah', 10);
+test.put('opry', 11);
+test.put('opine', 12);
+console.log('.keysWithPrefix(\'op\') check: ', JSON.stringify(test.keysWithPrefix('op'), null, 2));
+console.log('.longestPrefixOf(\'farthest\') check: ', test.longestPrefixOf('farthest'));
